9763ce5c0939355b4124614cffe9ef31f7266f85
Jesus <jesus.guetio@correounivalle.edu.co>
2025-02-24 16:00:48 -0500

    fuctions to parse the db schema

--- utils.py
+++ utils.py
@@ -31,40 +31,45 @@ def get_db_pk_schema():
     return primary_keys
 
 # Obtener claves foráneas
-cursor.execute("""
-    SELECT tc.table_name, kcu.column_name, ccu.table_name AS foreign_table, ccu.column_name AS foreign_column
-    FROM information_schema.table_constraints AS tc
-    JOIN information_schema.key_column_usage AS kcu
-    ON tc.constraint_name = kcu.constraint_name
-    JOIN information_schema.constraint_column_usage AS ccu
-    ON ccu.constraint_name = tc.constraint_name
-    WHERE tc.constraint_type = 'FOREIGN KEY' AND tc.table_schema = 'public'
-""")
-fk_info = cursor.fetchall()
+def get_db_fk_schema():
+    db = DBConnection()
+    db.generate_db_connection()
+    db.execute("""
+        SELECT tc.table_name, kcu.column_name, ccu.table_name AS foreign_table, ccu.column_name AS foreign_column
+        FROM information_schema.table_constraints AS tc
+        JOIN information_schema.key_column_usage AS kcu
+        ON tc.constraint_name = kcu.constraint_name
+        JOIN information_schema.constraint_column_usage AS ccu
+        ON ccu.constraint_name = tc.constraint_name
+        WHERE tc.constraint_type = 'FOREIGN KEY' AND tc.table_schema = 'public'
+    """)
+    fk_info = cursor.fetchall()
+    db.quit_db_connection()
+    return fk_info
 
-foreign_keys = {}
-for table, column, foreign_table, foreign_column in fk_info:
-    foreign_keys.setdefault(table, []).append(f'foreign_key: "{column}" {foreign_column} from "{foreign_table}" "{foreign_column}"')
 
-# Construcción del esquema en el formato que el modelo puede procesar
-tables = {}
-for table, column, col_type in columnas:
-    tables.setdefault(table, []).append(f'"{column}" {col_type}')
+#Organiza el esquema de la bd en el formato que el modelo puede procesar
+def parse_schema():
+    foreign_keys = {}
+    fk_info = get_db_fk_schema()
+    for table, column, foreign_table, foreign_column in fk_info:
+        foreign_keys.setdefault(table, []).append(f'foreign_key: "{column}" {foreign_column} from "{foreign_table}" "{foreign_column}"')
 
-schema_parts = []
-for table, columns in tables.items():
-    schema_line = f'"{table}" ' + " , ".join(columns)
-    if table in primary_keys:
-        schema_line += f' , primary key: "{primary_keys[table]}"'
-    if table in foreign_keys:
-        schema_line += " , " + " , ".join(foreign_keys[table])
-    schema_parts.append(schema_line)
+    tables = {}
+    columnas = get_db_columns_schema()
+    for table, column, col_type in columnas:
+        tables.setdefault(table, []).append(f'"{column}" {col_type}') 
 
-schema = ' [SEP] '.join(schema_parts)
+    schema_parts = []
+    primary_keys = get_db_pk_schema()
+    for table, columns in tables.items():
+        schema_line = f'"{table}" ' + " , ".join(columns)
+        if table in primary_keys:
+            schema_line += f' , primary key: "{primary_keys[table]}"'
+        if table in foreign_keys:
+            schema_line += " , " + " , ".join(foreign_keys[table])
+        schema_parts.append(schema_line)
 
-# Mostrar el esquema generado
-print(f'schema = """\n{schema}\n"""')
+    schema = ' [SEP] '.join(schema_parts)
 
-# Cerrar conexión
-cursor.close()
-conn.close()
+    return schema
