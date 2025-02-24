993880ffa300e726427521393f1a2207b7578e3e
Jesús Adrián Peña <95390821+JesusAdrianP@users.noreply.github.com>
2025-02-24 10:48:48 -0500

    initial commit testing the hugging face model interaction with fastapi

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+
+app = FastAPI()
+
+model_path = 'gaussalgo/T5-LM-Large-text2sql-spider'
+model = AutoModelForSeq2SeqLM.from_pretrained(model_path)
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+db_schema = """
+   "stadium" "Stadium_ID" int , "Location" text , "Name" text , "Capacity" int , "Highest" int , "Lowest" int , "Average" int , foreign_key:  primary key: "Stadium_ID" [SEP] "singer" "Singer_ID" int , "Name" text , "Country" text , "Song_Name" text , "Song_release_year" text , "Age" int , "Is_male" bool , foreign_key:  primary key: "Singer_ID" [SEP] "concert" "concert_ID" int , "concert_Name" text , "Theme" text , "Year" text , foreign_key: "Stadium_ID" text from "stadium" "Stadium_ID" , primary key: "concert_ID" [SEP] "singer_in_concert"  foreign_key: "concert_ID" int from "concert" "concert_ID" , "Singer_ID" text from "singer" "Singer_ID" , primary key: "concert_ID" "Singer_ID"
+"""
+
+class QueryInput(BaseModel):
+    query: str
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/translate/")
+async def translate_to_sql(input_data: QueryInput):
+    nl_query = input_data.query
+    input_text = " ".join(["Question: ",nl_query, "Schema:", db_schema])
+    model_inputs = tokenizer(input_text, return_tensors="pt")
+    outputs = model.generate(**model_inputs, max_length=512)
+    output_text = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    return {"sql_query": output_text[0]}
