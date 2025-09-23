a2b7ca6f11fac1c9407e0e3884d43d4a3f9e6f1d
Jesus <jesus.guetio@correounivalle.edu.co>
2025-09-22 22:50:44 -0500

    adding created date in user_db models

--- user_db/models.py
+++ user_db/models.py
@@ -1,5 +1,5 @@
 from api_root.api_db import Base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, func
 
 #creating the user_db model for the user_db table in the database
 class UserDB(Base):
@@ -12,3 +12,4 @@ class UserDB(Base):
     db_host = Column(String, index=True)
     db_schema = Column(String, index=True)
     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), index=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
