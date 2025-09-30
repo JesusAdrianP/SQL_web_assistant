fe7f6a8f08a763c4300c55b51537dcf3649b6ee3
Jesus <jesus.guetio@correounivalle.edu.co>
2025-09-30 17:45:51 -0500

    t5-ai-model enpoint connected

--- query/schemas.py
+++ query/schemas.py
@@ -3,9 +3,7 @@ from pydantic import BaseModel
 # Validation and serialization schemas for queries model
 class QueryBase(BaseModel):
     nl_query: str
-    sql_query: str
     is_correct: bool|None = None
-    user_id: int
     ai_model_id: int
     user_db_id: int
     
